67c6423bfc3732a117d4274755d4e32964c74f83
Nirvana <hello@nirvana.io>
2013-08-26 15:45:53 -0400

    wrote test, haven't tested yet.

--- mix.exs
+++ mix.exs
@@ -16,6 +16,9 @@ defmodule JsonShootout.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [
+      { :json, github: "cblage/elixir-json"}
+#      { :jazz, github: "meh/jazz"}
+    ]
   end
 end
